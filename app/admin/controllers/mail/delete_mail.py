5ddfa756ac2c5609d24b3410754f0ccbbc58122e
AlirezaieS <alirezaie@sadegh.org>
2020-05-27 08:39:16 +0430

    delete a mail

---
+++ app/admin/controllers/mail/delete_mail.py
@@ -0,0 +1,26 @@
+from django.views.generic import DeleteView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
+from alirezaies.models.contact_form_model import ContactForm
+
+@method_decorator(login_required, name='dispatch')
+class DeleteMail(DeleteView):
+    """
+    delete a mail
+    """
+    model = ContactForm
+    template_name = 'admin/mail/delete_mail.html'
+    success_url = reverse_lazy('mailbox')
+
+    def get_queryset(self):
+        query_set = super().get_queryset().filter(pk=self.kwargs['pk'])
+
+        return query_set
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete: ' + self.get_queryset()[0].subject
+
+        return context
--- app/admin/urls.py
+++ app/admin/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .controllers.main_admin_panel_page import main_admin_panel_func
 from admin.controllers.mail.mailbox import MailBoxView
 from admin.controllers.mail.mail_view import MailView
+from admin.controllers.mail.delete_mail import DeleteMail
+
+from .controllers.main_admin_panel_page import main_admin_panel_func
+
 
 urlpatterns = [
     path('', main_admin_panel_func, name='admin_dashboard'),
@@ -10,4 +13,5 @@ urlpatterns = [
     # ============ Mail ============
     path('mail/', MailBoxView.as_view(), name='mailbox'),
     path('mail/view/<int:pk>/', MailView.as_view(), name='mail_view'),
+    path('mail/delete/<int:pk>/', DeleteMail.as_view(), name='delete_mail'),
 ]
