5daf81055d7c4eeb808a5afe5c0a91a33d0a3ca6
AlirezaieS <alirezaie@sadegh.org>
2020-05-28 05:12:34 +0430

    add profile picture link to the models

--- app/alirezaies/models/bio_model.py
+++ app/alirezaies/models/bio_model.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Bio(models.Model):
+    image_url = models.CharField(max_length=2048, default='https://avatars3.githubusercontent.com/u/16784257?s=460&v=4')
     bio = models.TextField(max_length=4000)
 
     def __str__(self):
